29ee20e6644d5425176a2e81d75174563b1cf57a
z.zolfaghari <zahrazolfaghari00@gmail.com>
2024-07-09 11:07:16 +0330

    add requirements and change db config to postgres

--- OnlineShop/OnlineShop/settings.py
+++ OnlineShop/OnlineShop/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d+ibok0@^m-v@$7juwk3m-glxl6k#pilt9z1q#0ze0&-m1v&z&'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,13 +127,19 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME")
+,
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+        'OPTIONS': {
+           'options': os.getenv("DB_SCHEMA")
+        }
     }
 }
 
--- OnlineShop/main/shared/based_response/common_response.py
+++ OnlineShop/main/shared/based_response/common_response.py
@@ -8,7 +8,7 @@ class CreateResponse(Response):
     def __init__(self):
         self.default_message = 'Created Successfully'
         self.default_status_code = status.HTTP_201_CREATED
-        response = ResponseSerializer({'default_message': str(self.default_message), 'default_status_code': self.status_code})
+        response = ResponseSerializer({'message': str(self.default_message), 'status_code': self.status_code})
         super().__init__(data=response.data,
                          status=self.default_status_code)
 
